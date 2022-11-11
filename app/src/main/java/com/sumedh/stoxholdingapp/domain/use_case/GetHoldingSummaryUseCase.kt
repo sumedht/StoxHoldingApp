9cb7ca7792a906a8f3bcb6f2361c6abab841ebe3
sumedht <stambat5@gmail.com>
2022-11-12 00:18:04 +0530

    - Use inbuilt minus method. - Use Loading state with parameter

--- app/src/main/java/com/sumedh/stoxholdingapp/domain/use_case/GetHoldingSummaryUseCase.kt
+++ app/src/main/java/com/sumedh/stoxholdingapp/domain/use_case/GetHoldingSummaryUseCase.kt
@@ -1,11 +1,9 @@
 package com.sumedh.stoxholdingapp.domain.use_case
 
 import com.sumedh.stoxholdingapp.common.Resource
-import com.sumedh.stoxholdingapp.data.dto.toStock
 import com.sumedh.stoxholdingapp.domain.model.HoldingSummary
 import com.sumedh.stoxholdingapp.domain.repository.StockHoldingRepository
-import kotlinx.coroutines.flow.Flow
-import kotlinx.coroutines.flow.flow
+import kotlinx.coroutines.flow.*
 import javax.inject.Inject
 
 class GetHoldingSummaryUseCase @Inject constructor(
@@ -13,12 +11,11 @@ class GetHoldingSummaryUseCase @Inject constructor(
 ) {
     operator fun invoke() : Flow<Resource<HoldingSummary>> = flow {
         try {
-            emit(Resource.Loading())
-            val stocks = repository.getHoldingSummary().map { it.toStock() }
-
+            emit(Resource.Loading(true))
+            val stocks = repository.getHoldingSummary()
             val currentValue = stocks.fold(0.0) { currentVal, element -> currentVal + element.currentValue }
             val totalInvestment = stocks.fold(0.0) { totalInvestment, element -> totalInvestment + element.investmentValue}
-            val totalProfitAndLoss = currentValue - totalInvestment
+            val totalProfitAndLoss = currentValue.minus(totalInvestment)
             val todayProfitAndLoss = stocks.sumOf { (it.close - it.ltp) * it.quantity }
             emit(Resource.Success(HoldingSummary(currentValue, totalInvestment, totalProfitAndLoss, todayProfitAndLoss)))
         } catch (e: Exception) {
