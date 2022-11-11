e9a240ef574007a7d76878dbcd1d68f4b5fdb128
sumedht <stambat5@gmail.com>
2022-11-12 00:14:37 +0530

    Added flow to repository class

--- app/src/main/java/com/sumedh/stoxholdingapp/data/repository/StockHoldingRepositoryImpl.kt
+++ app/src/main/java/com/sumedh/stoxholdingapp/data/repository/StockHoldingRepositoryImpl.kt
@@ -1,27 +1,39 @@
 package com.sumedh.stoxholdingapp.data.repository
 
-import com.sumedh.stoxholdingapp.data.dto.Response
-import com.sumedh.stoxholdingapp.data.dto.StockDto
+import com.sumedh.stoxholdingapp.common.Resource
+import com.sumedh.stoxholdingapp.data.dto.toStock
 import com.sumedh.stoxholdingapp.data.remote.StockHoldingApi
-import com.sumedh.stoxholdingapp.domain.model.HoldingSummary
+import com.sumedh.stoxholdingapp.domain.model.Stock
 import com.sumedh.stoxholdingapp.domain.repository.StockHoldingRepository
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flow
+import retrofit2.HttpException
+import java.io.IOException
 import javax.inject.Inject
 
 class StockHoldingRepositoryImpl @Inject constructor(
     private val api: StockHoldingApi
 ) : StockHoldingRepository {
-    private var stocks : List<StockDto> = emptyList()
+    private var stocks : List<Stock> = emptyList()
 
-    override suspend fun getStocks(): Response {
-        val response = api.getStocks()
-        /* Storing value in local cache to satisfy use-case to return value for getHoldingSummary method.
-            This can be change to local data storage or remote API
-         */
-        stocks = response.data
-        return response
+    override suspend fun getStocks(): Flow<Resource<List<Stock>>> {
+
+        return flow {
+            try {
+                emit(Resource.Loading(true))
+                val response = api.getStocks()
+                response.error?.let { emit(Resource.Error(response.error.toString())) }
+                stocks = response.data.map { it.toStock() }
+                emit(Resource.Success(stocks))
+            } catch (e: HttpException) {
+                emit(Resource.Error(e.localizedMessage?:"An unexpected error occured"))
+            } catch (e: IOException) {
+                emit(Resource.Error("Couldn't reach server. Check your internet connectivity"))
+            }
+        }
     }
 
-    override suspend fun getHoldingSummary(): List<StockDto> {
-        return stocks;
-    }
+    override suspend fun getHoldingSummary(): List<Stock> {
+        return stocks
+        }
 }
